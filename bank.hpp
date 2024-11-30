f50357c364015f124423debc8680d74fbe27dc9b
Amyr14 <amyr.allan1@gamil.com>
2024-11-30 16:46:29 -0300

    Corrigido erro na função transfer. Adicionado guarda que verifica se a quantia é maior do que o saldo do emissor.

--- bank.hpp
+++ bank.hpp
@@ -34,6 +34,9 @@ string withdraw(BankState &bank_state, string withdrawer, int amount) {
 
 string transfer(BankState &bank_state, string sender, string receiver,
                 int amount) {
+  if ( bank_state.balances[sender] < amount )
+    return "Balance is too low";
+  
   bank_state.balances[sender] -= amount;
   bank_state.balances[receiver] += amount;
   return "";
